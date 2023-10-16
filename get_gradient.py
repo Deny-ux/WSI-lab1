b37fe524236c046702e0f9b00b5ea359f035288b
Deny-ux <denys.fokashchuk@gmail.com>
2023-10-16 12:59:42 +0200

    Added description for function which calculates gradient

--- get_gradient.py
+++ get_gradient.py
@@ -1,19 +1,22 @@
 import numpy as np
 from typing import Union
 
-def get_gradient_fx( point: Union[int, float]) -> float:
- 
-  if not (isinstance(point, int) or isinstance(point, float)):
-   raise TypeError("Only int or float can represent a point for function f(x)!")
-  return np.pi*np.cos(np.pi * point) + 2*point
+def get_gradient_fx(point: Union[int, float]) -> float:
+ """
+ Calculate gradient for function f(x) in specified point
+ """
+ if not (isinstance(point, int) or isinstance(point, float)):
+  raise TypeError("Only int or float can represent a point for function f(x)!")
+ return np.pi * np.cos(np.pi * point) + 2 * point
 
- 
-def get_gradient_gx(point: np.ndarray) -> float:
-
+def get_gradient_gx(point: np.ndarray) -> np.ndarray:
+ """
+ Calculate gradient for function g(x) in specified point
+ """
  if point.shape != (2,):
-  raise TypeError("Only ndarray with size 2x1 can represent a point for function g(x)!")
+  raise TypeError("Only ndarray with shape \"2,\" can represent a point for function g(x)!")
 
  x1 = point[0]
  x2 = point[1]
- return np.array([2*(x1 - 2 * np.e), 2* (x2 + np.e)])
+ return np.array([2 * (x1 - 2 * np.e), 2 * (x2 + np.e)])
  
