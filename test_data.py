af9098c822c763a60d0eeb53f4bf518e96697157
Deny-ux <denys.fokashchuk@gmail.com>
2023-10-16 14:32:25 +0200

    Added test data

--- test_data.py
+++ test_data.py
@@ -2,12 +2,7 @@ data_array_gx = [
  {
   "start_x1": -6,
   "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -10,
-  "learn_rate": 0.02
+  "learn_rate": 0.01
  },
  {
   "start_x1": -6,
@@ -17,190 +12,229 @@ data_array_gx = [
  {
   "start_x1": -6,
   "start_x2": -8,
-  "learn_rate": 0.02
+  "learn_rate": 0.1
  },
  {
   "start_x1": -6,
   "start_x2": -8,
-  "learn_rate": 0.02
+  "learn_rate": 0.5
  },
  {
   "start_x1": -6,
   "start_x2": -8,
+  "learn_rate": 1
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.00000009
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.09
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.01
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.2
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.5
+ },
+ {
+  "start_x1": -10,
+  "start_x2": 8,
+  "learn_rate": 0.6
+ },
+ {
+  "start_x1": 10,
+  "start_x2": 0,
+  "learn_rate": 0.000000000001
+ },
+ {
+  "start_x1": 10,
+  "start_x2": 0,
+  "learn_rate": 0.0000005
+ },
+ {
+  "start_x1": 10,
+  "start_x2": 0,
   "learn_rate": 0.02
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
+  "start_x1": 10,
+  "start_x2": 0,
+  "learn_rate": 0.6
+ },
+ {
+  "start_x1": 10,
+  "start_x2": 0,
+  "learn_rate": 3
+ },
+ {
+  "start_x1": 400,
+  "start_x2": -100,
+  "learn_rate": 0.000006
+ },
+ {
+  "start_x1": 400,
+  "start_x2": -100,
+  "learn_rate": 0.003
+ },
+ {
+  "start_x1": 400,
+  "start_x2": -100,
   "learn_rate": 0.02
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": 400,
+  "start_x2": -100,
+  "learn_rate": 0.07
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": 400,
+  "start_x2": -100,
+  "learn_rate": 0.999
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": -200.2,
+  "start_x2": -10.32,
+  "learn_rate": 0.0002
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": -200.2,
+  "start_x2": -10.32,
+  "learn_rate": 0.001
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": -200.2,
+  "start_x2": -10.32,
+  "learn_rate": 0.0786
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": -200.2,
+  "start_x2": -10.32,
+  "learn_rate": 0.849
  },
  {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
- },
- {
-  "start_x1": -6,
-  "start_x2": -8,
-  "learn_rate": 0.02
+  "start_x1": -200.2,
+  "start_x2": -10.32,
+  "learn_rate": 1.27
  },
 ]
 
 data_array_fx = [
  {
  "init_value": -6,
- "learn_rate": 0.02,
+ "learn_rate": 0.0000002,
  },
  {
   "init_value": -6,
-  "learn_rate": 0.1,
+  "learn_rate": 0.0001,
  },
  {
   "init_value": -6,
-  "learn_rate": 0.01,
+  "learn_rate": 0.03892,
  },
   {
   "init_value": -6,
-  "learn_rate": 0.01,
+  "learn_rate": 0.0938,
  },
   {
   "init_value": -6,
-  "learn_rate": 0.01,
+  "learn_rate": 0.999,
  },
   {
   "init_value": -4,
-  "learn_rate": 0.01,
+  "learn_rate": 0.0023211,
  },
   {
   "init_value": -4,
-  "learn_rate": 0.01,
+  "learn_rate": 0.01939,
  },
   {
   "init_value": -4,
-  "learn_rate": 0.01,
+  "learn_rate": 0.02232,
  },
   {
   "init_value": -4,
-  "learn_rate": 0.01,
+  "learn_rate": 0.292,
  },
   {
   "init_value": -4,
-  "learn_rate": 0.01,
+  "learn_rate": 0.608,
  },
   {
   "init_value": -2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.0089,
  },
  {
   "init_value": -2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.09,
  },
  {
   "init_value": -2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.423
  },
  {
   "init_value": -2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.849
  },
  {
   "init_value": -2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.9232
  },
  {
   "init_value": 0,
-  "learn_rate": 0.01,
+  "learn_rate": 0.02838
  },
  {
   "init_value": 0,
-  "learn_rate": 0.01,
+  "learn_rate": 0.084949,
  },
  {
   "init_value": 0,
-  "learn_rate": 0.01,
+  "learn_rate": 0.093929
  },
  {
   "init_value": 0,
-  "learn_rate": 0.01,
+  "learn_rate": 0.129923,
  },
  {
   "init_value": 0,
-  "learn_rate": 0.01,
+  "learn_rate": 0.43937
  },
  {
   "init_value": 0.77,
-  "learn_rate": 0.01,
+  "learn_rate": 0.02929
  },
  {
   "init_value": 0.77,
-  "learn_rate": 0.01,
+  "learn_rate": 0.059302
  },
  {
   "init_value": 0.77,
-  "learn_rate": 0.01,
+  "learn_rate": 0.6398
  },
  {
   "init_value": 0.77,
-  "learn_rate": 0.01,
+  "learn_rate": 0.92920
  },
  {
   "init_value": 0.77,
-  "learn_rate": 0.01,
+  "learn_rate": 1.4
+ },
+ {
+  "init_value": 2,
+  "learn_rate": 0.0000085,
  },
  {
   "init_value": 2,
@@ -208,18 +242,14 @@ data_array_fx = [
  },
  {
   "init_value": 2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.232,
  },
  {
   "init_value": 2,
-  "learn_rate": 0.01,
+  "learn_rate": 0.76,
  },
  {
   "init_value": 2,
-  "learn_rate": 0.01,
- },
- {
-  "init_value": 2,
-  "learn_rate": 0.01,
+  "learn_rate": 1.12,
  },
 ]
